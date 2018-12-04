c45ec64ad0bc12a6ec5cd4f694207f2e9700aed2
Youngsoo Han <amazingmobdev@gmail.com>
2018-12-05 01:38:38 +0900

    kakao login

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import RNKakaoLogins from "react-native-kakao-logins";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+interface State {
+  isKakaoLogging: boolean;
+  token: string;
+}
 
-export default class App extends Component {
+interface Props {}
+
+export default class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isKakaoLogging: false,
+      token: "token has not fetched"
+    };
+  }
+
+  // 카카오 로그인 시작.
+  kakaoLogin() {
+    console.log("   kakaoLogin   ");
+    RNKakaoLogins.login((err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("kakaoLogin", result);
+    });
+  }
+
+  kakaoLogout() {
+    console.log("   kakaoLogout   ");
+    RNKakaoLogins.logout((err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("kakaoLogout", result);
+    });
+  }
+
+  // 로그인 후 내 프로필 가져오기.
+  getProfile() {
+    console.log("getKakaoProfile");
+    RNKakaoLogins.getProfile((err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("getKakaoProfile", result);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View>
+          <TouchableOpacity
+            onPress={() => this.kakaoLogin()}
+            activeOpacity={0.5}
+          >
+            <Text>LOGIN</Text>
+          </TouchableOpacity>
+          <Text>{this.state.token}</Text>
+          <TouchableOpacity onPress={() => this.kakaoLogout()}>
+            <Text>LOGOUT</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.getProfile()}>
+            <Text>GETPROFILE</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -23,18 +79,8 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    alignItems: "center"
   }
 });
