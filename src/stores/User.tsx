89707fd172f520dd6e566dcccc5d04d3b6e75e95
Youngsoo Han <amazingmobdev@gmail.com>
2019-01-04 14:19:59 +0900

    UserStore

--- src/stores/User.tsx
+++ src/stores/User.tsx
@@ -2,17 +2,23 @@ import { observable, action } from "mobx";
 
 class UserStore {
   @observable monthSalary: number = 0;
-  @observable joinDate: string = "0";
-  @observable salaryDate: string = "0";
+  @observable joinDate: Date = new Date();
+  @observable salaryDate: Date = new Date();
+  @observable workStartTime: number = new Date().getTime();
+  @observable workEndTime: number = new Date().getTime();
 
   @action setUserInfo = (
     monthSalary: number,
-    joinDate: string,
-    salaryDate: string
+    joinDate: Date,
+    salaryDate: Date,
+    workStartTime: number,
+    workEndTime: number
   ) => {
     this.monthSalary = monthSalary;
     this.joinDate = joinDate;
     this.salaryDate = salaryDate;
+    this.workStartTime = workStartTime;
+    this.workEndTime = workEndTime;
   };
 }
 
