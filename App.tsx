c6756c18b893198fd335b7823b8b8d43f60cf8aa
Youngsoo Han <amazingmobdev@gmail.com>
2019-01-21 17:35:19 +0900

    add images

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import { Provider } from "mobx-react";
 
-import stores from "./src/stores/";
+import stores from "./src/stores";
 import AppNavigator from "./src/navigators/AppNavigator";
 import NavigatoinService from "./src/NavigatoinService";
+import { LocalStorage, USER } from "./src/utils/LocalStorage";
 
 class App extends Component {
-  componentDidMount = () => {
-    setTimeout(() => {
-      NavigatoinService.push("WithIntro");
-    }, 2000);
+  componentDidMount = async () => {
+    const userInfoExist: Array<any> = await Promise.all([
+      LocalStorage.getItem(USER),
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, 3000);
+      })
+    ]);
+
+    if (userInfoExist[0]) {
+      NavigatoinService.push("WithoutIntro");
+      return;
+    }
+
+    NavigatoinService.push("WithIntro");
   };
 
   render() {
