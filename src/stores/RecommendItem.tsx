4eeeec2312844e0ab9011d58cd9072db426b9fa8
Youngsoo Han <amazingmobdev@gmail.com>
2019-01-29 01:17:47 +0900

    월급에 따라 추천 구매 물품을 가져오는 서버로직 패치

---
+++ src/stores/RecommendItem.tsx
@@ -0,0 +1,64 @@
+import { observable, action } from "mobx";
+
+import agent from "../lib/agent";
+
+export type RecommendItemType = {
+  id: number;
+  name: string;
+  url: string;
+  price: number;
+};
+
+class RecommendItemStore {
+  @observable dayRecommendItems: RecommendItemType[] = [];
+  @observable monthRecommendItems: RecommendItemType[] = [];
+  @observable yearRecommendItems: RecommendItemType[] = [];
+
+  @action fetchDayRecommendItems = async (price: number) => {
+    try {
+      const dayRecommendItems = await agent.requests.getWithQuery(
+        "",
+        "/items",
+        {
+          price
+        }
+      );
+      this.dayRecommendItems = dayRecommendItems;
+    } catch (error) {
+      console.log("fetchDayRecommendItems error");
+    }
+  };
+
+  @action fetchMonthRecommendItems = async (price: number) => {
+    try {
+      const monthRecommendItems = await agent.requests.getWithQuery(
+        "",
+        "/items",
+        {
+          price
+        }
+      );
+      this.monthRecommendItems = monthRecommendItems;
+    } catch (error) {
+      console.log("fetchDayRecommendItems error");
+    }
+  };
+
+  @action fetchYearRecommendItems = async (price: number) => {
+    try {
+      const yearRecommendItems = await agent.requests.getWithQuery(
+        "",
+        "/items",
+        {
+          price
+        }
+      );
+      this.yearRecommendItems = yearRecommendItems;
+    } catch (error) {
+      console.log("fetchDayRecommendItems error");
+    }
+  };
+}
+
+export const RecommendItem = new RecommendItemStore();
+export type RecommendItemStoreType = typeof RecommendItem;
