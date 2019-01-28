1103666db2747cb8d34f7fd0f96e0ad30c247db6
Youngsoo Han <amazingmobdev@gmail.com>
2019-01-29 01:18:23 +0900

    export RecommendItem Store

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -1,7 +1,9 @@
 import { User } from "./User";
 import { Salary } from "./Salary";
+import { RecommendItem } from "./RecommendItem";
 
 export default {
   User,
-  Salary
+  Salary,
+  RecommendItem
 };
